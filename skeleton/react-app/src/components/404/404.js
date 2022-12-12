4f7f555c1cd93510dec50ce50be0b42b4e549236
Brian Kiesel <app_academy_student_bk@protonmail.com>
2022-12-12 13:48:23 -0800

    starting the 404

---
+++ skeleton/react-app/src/components/404/404.js
@@ -0,0 +1,20 @@
+
+
+export default function FourOFour() {
+  return (
+    <div>
+      <div id="title-bar">
+        <img src="/svg/habitica_logo.svg"></img>
+        <img src="routinica_proj_logo.png"></img>
+      </div>
+      <div id="four-o-four-png-div">
+        <img src="404.routinica.png" alt="A 404 on a 404!!!"></img>
+      </div>
+      <h1>Sometimes even the bravest adventurer gets lost.</h1>
+      <p>
+        Looks like this link is broken or the page may have moved, sorry!
+      </p>
+      <div>Head back to the <button>Homepage</button> or <button>Contact Us</button> about the issue.</div>
+    </div>
+  )
+}
