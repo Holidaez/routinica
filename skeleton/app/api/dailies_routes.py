29def634ef9649750ad008654a9e0e52bacd1c37
Dave Sexton <davesextonjr@gmail.com>
2022-12-12 09:59:52 -0500

    complete get user logic

--- skeleton/app/api/dailies_routes.py
+++ skeleton/app/api/dailies_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from ..models import Dailies, db, User
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 dailies_routes = Blueprint('dailies', __name__)
 
@@ -12,3 +12,26 @@ def get_all_dailies():
     """
     dalies = Dailies.query.filter(Dailies.userId == User.id).order_by(Dailies.display_order)
     return {'dailies': [daily.to_dict() for daily in dalies]}
+
+@dailies_routes.route('/test')
+@login_required
+def get_user_test():
+    """
+    Testing Route for relationships
+    """
+    return_user = current_user.to_dict()
+    dailies = {}
+    habits = {}
+    todos = {}
+
+    for daily in current_user.dailies:
+        dailies[daily.id] = daily.to_dict()
+
+    for habit in current_user.habits:
+        habits[habit.id] = habit.to_dict()
+
+    for todo in current_user.todos:
+        todos[todo.id] = todo.to_dict()
+
+    current_state = {"user": return_user, "dailies": dailies, "habits": habits, "todos": todos}
+    return current_state
