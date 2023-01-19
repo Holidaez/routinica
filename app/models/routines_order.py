3e2491c2909604c819b4159f1e1d5766d773a7f8
David Sexton Jr <davesextonjr@gmail.com>
2023-01-19 16:28:49 -0500

    test new table

--- app/models/routines_order.py
+++ app/models/routines_order.py
@@ -1,18 +1,18 @@
 from .db import db, SCHEMA, environment, add_prefix_for_prod
 
-class RoutinesOrder:
+class RoutinesOrder(db.Model):
     __tablename__ = 'routines_order'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-        id = db.Column(db.Integer, primary_key=True)
-        userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
-        daily_order = db.Column(db.String)
-        todo_order = db.Column(db.String)
-        habit_order = db.Column(db.String)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    daily_order = db.Column(db.String)
+    todo_order = db.Column(db.String)
+    habit_order = db.Column(db.String)
 
-        user = db.relationship('User', back_populates='order')
+    user = db.relationship('User', back_populates='order')
 
     def to_dict(self):
 
